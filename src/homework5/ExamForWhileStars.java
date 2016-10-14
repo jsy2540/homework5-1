e8b8f147ea4315f0f145ea3f3faf45c035e60df9
201344033 <jsy2540@naver.com>
2016-10-14 15:43:56 +0900

    ㄱㄱ

--- src/homework5/ExamForWhileStars.java
+++ src/homework5/ExamForWhileStars.java
@@ -3,7 +3,8 @@ package homework5;
 public class ExamForWhileStars {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("æ»≥Á«œººø‰");
+		
 
 	}
 
